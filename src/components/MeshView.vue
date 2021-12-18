8ed18e6b17f2e1e3181dca7f82637bdc4906691b
cain <cainmaila@gmail.com>
2021-12-18 14:40:22 +0800

    setting taiwind

---
+++ src/components/MeshView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <div>MeshView</div>
+</template>
+
+<style lang="postcss" scoped>
+a {
+  color: #42b983;
+}
+</style>
