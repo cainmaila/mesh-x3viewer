26f822148eda457257a8e8f2614e24ccd0b8d76f
cain <cainmaila@gmail.com>
2021-12-18 14:07:18 +0800

    add https

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+const fs = require("fs");
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  server: {
+    https: {
+      key: fs.readFileSync(
+        path.resolve(process.env.SSL_KEY_FILE || "localhost-key.pem")
+      ),
+      cert: fs.readFileSync(
+        path.resolve(process.env.SSL_CERT_FILE || "localhost.pem")
+      ),
+    },
+  },
+});
