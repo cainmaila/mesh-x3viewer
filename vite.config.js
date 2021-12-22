699da603040e0ae29b35d31616ee4699c6795169
cain <cainmaila@gmail.com>
2021-12-22 15:13:27 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,20 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-const fs = require("fs");
-const path = require("path");
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+const fs = require('fs')
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [vue()],
   server: {
     https: {
       key: fs.readFileSync(
-        path.resolve(process.env.SSL_KEY_FILE || "localhost-key.pem")
+        path.resolve(process.env.SSL_KEY_FILE || 'localhost-key.pem'),
       ),
       cert: fs.readFileSync(
-        path.resolve(process.env.SSL_CERT_FILE || "localhost.pem")
+        path.resolve(process.env.SSL_CERT_FILE || 'localhost.pem'),
       ),
     },
   },
-});
+})
