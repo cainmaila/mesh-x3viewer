f9fc534d66cada2ab348785ef4063f7dc3fde2de
cain <cainmaila@gmail.com>
2022-01-12 19:48:06 +0800

    說明

---
+++ src/components/Readme.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="Readme" class="absolute inset-2/4 bg-white/90">Readme</div>
+</template>
+<script>
+export default {
+  name: 'Readme',
+  data() {
+    return {}
+  },
+}
+</script>
+<style lang="postcss" scoped>
+#Readme {
+  width: 300px;
+  height: 300px;
+  transform: translate3d(-50%, -50%, 0);
+  border-radius: 10px;
+  padding: 10px;
+}
+</style>
